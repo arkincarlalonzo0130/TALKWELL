cedd6558ba5e32bdfdb07701bd4b25e507b19977
ARKIN CARL <arkincarlalonzo0130@gmail.com>
2026-09-07 14:19:38 +0800

    -Added java class -Coded profile for "Alonzo"

--- app/src/test/java/com/example/talkwell/quarter2/Alonzo_MiniPeta1.java
+++ app/src/test/java/com/example/talkwell/quarter2/Alonzo_MiniPeta1.java
@@ -1,4 +1,20 @@
 package com.example.talkwell.quarter2;
 
+import org.junit.Test;
+
 public class Alonzo_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myName = "Arkin";
+        String petName = "Oreo";
+        String favFood = "Chicken";
+        int myAge = 16;
+
+        // --- 2. THE OUTPUT (Printing to the console)
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
 }
