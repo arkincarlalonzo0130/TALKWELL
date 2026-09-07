43387031ff22624ea0b549cd9d6547485ef64e40
Aloizius M Napari <oyonapari08@gmail.com>
2026-09-07 14:39:09 +0800

    Napari's some personal information about His life added class.

--- app/src/test/java/com/example/talkwell/quarter2/Napari_MiniPeta1.java
+++ app/src/test/java/com/example/talkwell/quarter2/Napari_MiniPeta1.java
@@ -1,4 +1,20 @@
 package com.example.talkwell.quarter2;
 
+import org.junit.Test;
+
 public class Napari_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myName = "Napari";
+        String petName = "Pillow";
+        String favFood = "Alfredo";
+        int myAge = 16;
+
+        // --- 2. THE OUTPUT (Printing to console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wondeful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
 }
