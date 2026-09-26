cea846b91558024f8fac561439c63f265bc4a1fc
Arkin <arkincarlalonzo0130@gmail.com>
2026-09-27 00:29:43 +0800

    2nd Commit: Added the start of the backend code for the Fast Food Junit Test. The start contains the importing of the Scanner needed to read user inputs, the while loop to ensure the program keeps running, the code to print out the fast food menu that the user can choose from, and then the line of code to make it so the Scanner will read the next integer input by the user.

--- app/src/test/java/com/example/talkwell/quarter2/PracticalExam/Alonzo_FastFoodMenu.java
+++ app/src/test/java/com/example/talkwell/quarter2/PracticalExam/Alonzo_FastFoodMenu.java
@@ -1,8 +1,22 @@
 package com.example.talkwell.quarter2.PracticalExam;
 
+import com.example.talkwell.quarter2.PracticalExam;
+
 import java.util.Scanner;
 public class Alonzo_FastFoodMenu {
-    public void static main(String[] args) {
+    public void start(Scanner scanner) {
+
+        boolean running = true;
+
+        while (running) {
+
+                System.out.println("=== FAST FOOD MENU===");
+                System.out.println("1. Burger");
+                System.out.println("2. Fries");
+                System.out.println("3. I would not like to order");
+                System.out.print("Welcome to Fast Food! What would you like to order?:");
+
+                int choice = scanner.nextInt();
 
     }
 }
